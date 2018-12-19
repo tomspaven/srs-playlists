b176288a2890687340928a30b3bb836fc8577c4b
tomspaven <tomspaven@gmail.com>
2018-12-19 00:11:00 +0000

    Minor refactoring of leitner for cleanliness

--- pkg/srs/leitner.go
+++ pkg/srs/leitner.go
@@ -11,15 +11,23 @@ type normalisedLeitner struct {
 
 func (nl *normalisedLeitner) calculateNext(card Card, ease EaseFactor) (nextVals, error) {
 	if card.metaData.interval == 0 {
-		return nextVals{
-			time.Now().Add(nl.initInterval),
-			nl.initInterval,
-		}, nil
+		return nl.initialNextVals(), nil
 	}
 
-	nextInterval := time.Duration(float64(nl.initInterval) * math.Pow(float64(ease), float64(card.metaData.numberReviews)))
+	nextInterval := nl.calculateNextInterval(card, ease)
 	return nextVals{
 		due:      time.Now().Add(nextInterval),
 		interval: nextInterval,
 	}, nil
+}
+
+func (nl *normalisedLeitner) initialNextVals() nextVals {
+	return nextVals{
+		due:      time.Now().Add(nl.initInterval),
+		interval: nl.initInterval,
+	}
+}
+
+func (nl *normalisedLeitner) calculateNextInterval(card Card, ease EaseFactor) time.Duration {
+	return time.Duration(float64(nl.initInterval) * math.Pow(float64(ease), float64(card.metaData.numberReviews)))
 }
